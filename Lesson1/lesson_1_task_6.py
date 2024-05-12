847fdaafac05fb420a7acc89a318e8ef380d9df0
Rustam71 <rustam-sajfullin@mail.ru>
2024-05-13 00:54:25 +0300

    Выполнение 8 заданий

---
+++ Lesson1/lesson_1_task_6.py
@@ -0,0 +1,31 @@
+def num1():
+    print(1, end="")
+def num2():
+    print(2, end="")
+def num3():
+    print(3, end="")
+def num4():
+    print(4, end="")
+def num5():
+    print(5, end="")
+def num6():
+    print(6, end="")
+def num7():
+    print(7, end="")
+def num8():
+    print(8, end="")
+def num9():
+    print(9, end="")
+def num0():
+    print(0, end="")
+num8()
+num8()
+num0()
+num0()
+num5()
+num5()
+num5()
+num3()
+num5()
+num3()
+num5()
---
+++ Lesson1/lesson_1_task_7.py
@@ -0,0 +1,14 @@
+def numbers(number):
+    print(number, end='')
+numbers(8)
+numbers(8)
+numbers(0)
+numbers(0)
+numbers(5)
+numbers(5)
+numbers(5)
+numbers(3)
+numbers(5)
+numbers(3)
+numbers(5)
+
